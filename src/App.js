309b7307986cc5c60e7062d9ea69f3f7c73529d6
Yenhj <yen.greact@gmail.com>
2022-12-15 18:02:22 +0900

    axios 추가 및 useMatch 적용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+// axios API
+import instance from "./api/axios";
+import requests from "./api/request";
+
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -15,67 +19,27 @@ const App = () => {
   // 배열은 [state, state업데이트함수] 돌려받는다.
   // 배열은 [getter, setter] 돌려받는다.
   // useState(초기값) : state의 초기값
-  const [members, setMember] = useState([
-    { name: "Justin Bieber", photo: "photos/Justin Biber.jpg" },
-    { name: "Lauv", photo: "photos/Lauv.jpg" },
-    { name: "Keshi", photo: "photos/Keshi.jpg" },
-    { name: "Drake", photo: "photos/Drake.jpg" },
-    { name: "Johnny Pike", photo: "photos/JPike.jpg" },
-    { name: "Toby Ruckert", photo: "photos/Toby.jpg" },
-  ]);
+  const [members, setMembers] = useState([]);
+  const [songs, setSongs] = useState([]);
+
+  // 외부데이터 가져오기
+  const fetchData = async () => {
+    // 멤버목록 가져오기
+    const resultMember = await instance.get(requests.fetchMember);
+    setMembers(resultMember.data);
+    // 노래목록 가져오기
+    const resultSong = await instance.get(requests.fetchSong);
+    setSongs(resultSong.data);
+
+    // setMembers();
+    // setSongs();
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   // 플레이 리스트 State
   // 화면을 리랜더링 할 수 있는 조건은 state / props 변경
-  const [songs, setSongs] = useState([
-    {
-      id: 1,
-      title: "Peaches",
-      musician: "Justin Bieber",
-      youtube_link: "tQ0yjYUFKAE",
-    },
-    {
-      id: 2,
-      title: "Never not",
-      musician: "Lauv",
-      youtube_link: "p6owUZ_pExE",
-    },
-    {
-      id: 3,
-      title: "Summer",
-      musician: "Keshi",
-      youtube_link: "6B00t5dqIPU-A",
-    },
-    {
-      id: 4,
-      title: "God's Plan",
-      musician: "Drake",
-      youtube_link: "m1a_GqJf02M",
-    },
-    {
-      id: 5,
-      title: "Doctor my eyes",
-      musician: "Jackson Browne",
-      youtube_link: "7JlFKS_1oZk",
-    },
-    {
-      id: 6,
-      title: "We gotta get you a woman",
-      musician: "Todd Rundgren",
-      youtube_link: "EyUjbBViAGE",
-    },
-    {
-      id: 7,
-      title: "Hip to my heart",
-      musician: "Band Perry",
-      youtube_link: "vpLCFnD9LFo",
-    },
-    {
-      id: 8,
-      title: "Rolling in the deep",
-      musician: "Adele",
-      youtube_link: "EvK8pDK6IQU",
-    },
-  ]);
 
   return (
     <Router>
